f7fee7ada50fe03a1bab8da4fc5537a36c13fa23
fallenleaves256 <9261562+fallenleavesguy@users.noreply.github.com>
2024-11-17 13:23:06 +0800

    refactor: change hard code data to data source interface

---
+++ SimpleApp.Tests/Comparer.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleApp.Tests
+{
+    public class Comparer {
+        public delegate bool ComparisonDelegate<T>(T? x, T? y);
+
+        public static Comparer<U?> Get<U>(ComparisonDelegate<U?> func) => new Comparer<U?>(func);
+    }
+
+    public class Comparer<T> : Comparer, IEqualityComparer<T>
+    {
+        private ComparisonDelegate<T> comparisonFunction;
+
+        public Comparer(ComparisonDelegate<T> func)
+        {
+            comparisonFunction = func;
+        }
+
+        public bool Equals(T? x, T? y)
+        {
+            return comparisonFunction(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj?.GetHashCode() ?? 0;
+        }
+    }
+}
